ff91678a9bd1ed56904203ec1539537a6ebc57ee
Sev4ik <se8v7a@gmail.com>
2023-08-24 17:30:09 +0300

    update contactlist

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -4,9 +4,7 @@ import ContactItem from '../ContactItem/ContactItem';
 import './ContactList.css';
 import {
   addNewContact,
-  selectContact,
   getContactsAsync,
-  deleteContactAsync, 
 } from '../../store/slices/contactSlice';
 
 function ContactList() {
@@ -22,14 +20,6 @@ function ContactList() {
     dispatch(addNewContact());
   }
 
-  const onDeleteContact = (id) => {
-
-    dispatch(deleteContactAsync(id)); 
-  }
-
-  const onEditContact = (contact) => {
-    dispatch(selectContact(contact));
-  }
 
   return (
     <div className='list-container'>
@@ -38,8 +28,6 @@ function ContactList() {
           <ContactItem
             key={contact.id}
             contact={contact}
-            onDelete={onDeleteContact}
-            onEdit={onEditContact}
           />
         ))}
       </div>
